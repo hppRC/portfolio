48bb0736928c207758b5da5b3d782453bd6b5bc0
hppRC <hpp.ricecake@gmail.com>
2019-09-17 04:10:55 +0900

    test

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,103 +1,121 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { kebabCase } from 'lodash'
-import Helmet from 'react-helmet'
-import { graphql, Link } from 'gatsby'
-import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { kebabCase } from 'lodash';
+import Helmet from 'react-helmet';
+import { graphql, Link } from 'gatsby';
+import Layout from '../components/Layout';
+import Content, { HTMLContent } from '../components/Content';
+
+const Meta = ({ post }) => {
+	const origin = 'http://hpprc.com';
+
+	return (
+		<Helmet
+			title={`${post.frontmatter.title} | Blog`}
+			meta={[
+				{ name: 'description', content: post.frontmatter.description },
+				{ property: 'og:title', content: post.frontmatter.title },
+				{
+					property: 'og:description',
+					content: post.frontmatter.description
+				},
+				{
+					property: 'og:image',
+					content: `${origin}${post.frontmatter.image}`
+				}
+			]}
+		/>
+	);
+};
 
 export const BlogPostTemplate = ({
-  content,
-  contentComponent,
-  description,
-  tags,
-  title,
-  helmet,
+	content,
+	contentComponent,
+	description,
+	tags,
+	title,
+	helmet
 }) => {
-  const PostContent = contentComponent || Content
+	const PostContent = contentComponent || Content;
 
-  return (
-    <section className="section">
-      {helmet || ''}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map(tag => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-          </div>
-        </div>
-      </div>
-    </section>
-  )
-}
+	return (
+		<section className='section'>
+			{helmet || ''}
+			<div className='container content'>
+				<div className='columns'>
+					<div className='column is-10 is-offset-1'>
+						<h1 className='title is-size-2 has-text-weight-bold is-bold-light'>
+							{title}
+						</h1>
+						<p>{description}</p>
+						<PostContent content={content} />
+						{tags && tags.length ? (
+							<div style={{ marginTop: `4rem` }}>
+								<h4>Tags</h4>
+								<ul className='taglist'>
+									{tags.map(tag => (
+										<li key={tag + `tag`}>
+											<Link
+												to={`/tags/${kebabCase(tag)}/`}
+											>
+												{tag}
+											</Link>
+										</li>
+									))}
+								</ul>
+							</div>
+						) : null}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
 
 BlogPostTemplate.propTypes = {
-  content: PropTypes.node.isRequired,
-  contentComponent: PropTypes.func,
-  description: PropTypes.string,
-  title: PropTypes.string,
-  helmet: PropTypes.object,
-}
+	content: PropTypes.node.isRequired,
+	contentComponent: PropTypes.func,
+	description: PropTypes.string,
+	title: PropTypes.string,
+	helmet: PropTypes.object
+};
 
 const BlogPost = ({ data }) => {
-  const { markdownRemark: post } = data
+	const { markdownRemark: post } = data;
 
-  return (
-    <Layout>
-      <BlogPostTemplate
-        content={post.html}
-        contentComponent={HTMLContent}
-        description={post.frontmatter.description}
-        helmet={
-          <Helmet titleTemplate="%s | Blog">
-            <title>{`${post.frontmatter.title}`}</title>
-            <meta
-              name="description"
-              content={`${post.frontmatter.description}`}
-            />
-          </Helmet>
-        }
-        tags={post.frontmatter.tags}
-        title={post.frontmatter.title}
-      />
-    </Layout>
-  )
-}
+	return (
+		<Layout>
+			<BlogPostTemplate
+				content={post.html}
+				contentComponent={HTMLContent}
+				description={post.frontmatter.description}
+				helmet={<Meta post={post} />}
+				tags={post.frontmatter.tags}
+				title={post.frontmatter.title}
+			/>
+		</Layout>
+	);
+};
 
 BlogPost.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.object,
-  }),
-}
+	data: PropTypes.shape({
+		markdownRemark: PropTypes.object
+	})
+};
 
-export default BlogPost
+export default BlogPost;
 
 export const pageQuery = graphql`
-  query BlogPostByID($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      id
-      html
-      frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        title
-        description
-        tags
-      }
-    }
-  }
-`
+	query BlogPostByID($id: String!) {
+		markdownRemark(id: { eq: $id }) {
+			id
+			html
+			frontmatter {
+				date(formatString: "MMMM DD, YYYY")
+				title
+				description
+				tags
+			}
+		}
+	}
+`;
