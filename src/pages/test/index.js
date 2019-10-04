63ad7a77daa0c48ab4a4fb06ed38f8c9fad8de71
hppRC <hpp.ricecake@gmail.com>
2019-10-04 23:41:04 +0900

    📄testとしてページを作ってみた！

---
+++ src/pages/test/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+const Test = () => {
+    return (
+        <Layout>
+            <h1>test</h1>
+        </Layout>
+    )
+}
+
+export default Test;
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, graphql } from 'gatsby';
-
 import Layout from '../components/Layout';
 import Features from '../components/Features';
 import BlogRoll from '../components/BlogRoll';
+
+
 
 export const IndexPageTemplate = ({
 	image,
