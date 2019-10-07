d6f11367968705559cef098b803f83e9adb2ab5f
hppRC <hpp.ricecake@gmail.com>
2019-10-07 09:16:11 +0900

    😌なんもわからん

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,11 +41,30 @@ module.exports = {
             ...UIComponents
           }
         `,
-
-        preloadedMDXScope: `import { UIComponents } from './src/Theme'`,
-
         // mdPlugins: [],
-        // gatsbyRemarkPlugins: [{}],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: "gatsby-remark-relative-images",
+            options: {
+              name: "uploads",
+            },
+          },
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 2048,
+            },
+          },
+          {
+            resolve: "gatsby-remark-copy-linked-files",
+            options: {
+              destinationDir: "static",
+            },
+          },
+        ],
       },
     },
     {
--- src/Boot.tsx
+++ src/Boot.tsx
@@ -1,7 +1,7 @@
 import { MDXProvider } from "@mdx-js/tag"
 import React from "react"
 import { Provider as StateProvider } from "unstated"
-import { LayoutComponents, Theme, UIComponents } from "./Theme"
+import { LayoutComponents, Theme } from "./Theme"
 
 // TODO: Need to create gatsby-plugin-react-head
 // import { HeadProvider } from "react-head"
@@ -9,7 +9,7 @@ import { LayoutComponents, Theme, UIComponents } from "./Theme"
 export const Boot: React.SFC<{ element: any }> = ({ element }) => {
   return (
     <StateProvider>
-      <MDXProvider components={{ ...LayoutComponents, ...UIComponents }}>
+      <MDXProvider components={LayoutComponents}>
         <Theme>{element}</Theme>
       </MDXProvider>
     </StateProvider>
