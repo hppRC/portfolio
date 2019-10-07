7fbda61cfe107b6d7616690435ea3670b688c549
hppRC <hpp.ricecake@gmail.com>
2019-10-08 02:26:49 +0900

    image.jsなどをtypescript移行

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export * from "./call-to-action.component"
-export * from "./heading.component"
-export * from "./smart-link.component"
-export * from "./header"
-export * from "./seo"
-export * from "./image"
-export * from "./menu.component"
