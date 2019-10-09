0deab0bf597aef4c4e5d92e6229f06ffd627a36c
hppRC <hpp.ricecake@gmail.com>
2019-10-10 01:44:13 +0900

    windowを使わないように変更

--- src/keyVisuals/top.js
+++ src/keyVisuals/top.js
@@ -22,9 +22,9 @@ varying vec3 pos;
 void main() {
 	vColor = color;
 	pos = vec3(
-		3. * position.x * ( 1. / time + cos(randx + position.x) * position.x * sin(1. + position.z * time) * cos(1.2 * position.x * sin(cos(position.z + 0.01 * sin(0.5 * time + randx))) + sin(cos(0.5 * time) * position.y))),
-		3. * position.y * (1. / time + sin(randy + position.z + 0.5 * time - randy) * position.y * cos(0.001*0.5 * time + position.z + sin(cos(sin(1. - sin(0.5 * time) * cos(0.5 * time)) + sin(0.5 * time)) + sin(cos(0.5 * time)) + 0.5 * time * sin(position.y))) * cos(position.x * cos(sin(0.5 * time)))),
-		3. * position.z * (1. / abs(0.5 * time - 100.) + cos(randz + 0.0025*0.5 * time) * position.z + sin(sin(position.y * sin(cos(0.5 * time) + 0.5 * time * sin(position.y)) * cos(position.x * cos(sin(0.5 * time))))))
+		position.x * ( 1. / time + cos(randx + position.x) * position.x * sin(1. + position.z * time) * cos(1.2 * position.x * sin(cos(position.z + 0.01 * sin(0.5 * time + randx))) + sin(cos(0.5 * time) * position.y))),
+		position.y * (1. / time + sin(randy + position.z + 0.5 * time - randy) * position.y * cos(0.001*0.5 * time + position.z + sin(cos(sin(1. - sin(0.5 * time) * cos(0.5 * time)) + sin(0.5 * time)) + sin(cos(0.5 * time)) + 0.5 * time * sin(position.y))) * cos(position.x * cos(sin(0.5 * time)))),
+		position.z * (1. / abs(0.5 * time - 100.) + cos(randz + 0.0025*0.5 * time) * position.z + sin(sin(position.y * sin(cos(0.5 * time) + 0.5 * time * sin(position.y)) * cos(position.x * cos(sin(0.5 * time))))))
 		);
 	gl_PointSize = 1.5;
 	gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
@@ -40,9 +40,9 @@ varying vec4 vColor;
 varying vec3 pos;
 void main() {
 	gl_FragColor = vec4(
-		vColor.r * abs(sin(time + pos.z) + cos(pos.x)),
-		vColor.g * abs(cos(time - vColor.b) + sin(pos.z)),
-		vColor.b * abs(sin(time - pos.x) / abs(sin(time - vColor.b))),
+		vColor.r * abs(sin(time*0.4 + pos.z) + pos.x),
+		vColor.g * abs(cos(time*0.4 - vColor.b) + pos.z),
+		vColor.b * abs(sin(time*0.4 - pos.x) / abs(sin(time*0.4 - vColor.b)) + abs(cos(time*0.4 - vColor.r))),
 		1.0
 		);
 }
@@ -126,12 +126,7 @@ const TopArt = () => (
 	<div css={theme}>
 		<Canvas
 			camera={{
-				position: [
-					0,
-					0,
-					1 + typeof window !== 'undefined' &&
-						window.innerHeight / window.innerWidth
-				]
+				position: [0, 0, 1]
 			}}
 			shadowMap
 		>
