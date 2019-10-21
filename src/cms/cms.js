02266d4e239de4a153568becd873b05495d078c3
hppRC <hpp.ricecake@gmail.com>
2019-10-21 13:41:19 +0900

    markdownがダメだったのでやりなおし

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,46 +1,8 @@
 import React from 'react';
 import CMS from 'netlify-cms-app';
-import { MdxControl, setupPreview } from 'netlify-cms-widget-mdx';
 import * as mdxComponents from '../mdxComponents';
+import Preview from './preview';
 
-const components = {
-	h1: ({ children, ...props }) => (
-		<h1 style={{ color: 'tomato' }} {...props}>
-			{children}
-		</h1>
-	)
-};
-
-//scope
-
-const scope = {
-	LayoutTest: props => (
-		<div
-			style={{
-				padding: '10px',
-				border: '1px solid green',
-				borderRadius: '5px'
-			}}
-			{...props}
-		/>
-	),
-	Test: () => <h1>Testなのだ</h1>,
-	...mdxComponents
-};
-
-console.log(scope);
-
-const allowedImports = {};
-
-CMS.registerWidget(
-	'mdx',
-	MdxControl,
-	setupPreview({
-		components: components,
-		scope: scope,
-		allowedImports: allowedImports,
-		mdPlugins: []
-	})
-);
+CMS.registerPreviewTemplate('posts', Preview);
 
 CMS.init();
---
+++ src/cms/preview.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Preview = () => <h1>Preview Test</h1>;
+
+export default Preview;
