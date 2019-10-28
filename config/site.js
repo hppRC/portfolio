7415f9fff3dfcb74b69ca26eed8faee3d9f2612c
hppRC <hpp.ricecake@gmail.com>
2019-10-29 00:48:32 +0900

    hooksの導入

--- config/site.js
+++ config/site.js
@@ -1,16 +1,15 @@
 module.exports = {
 	pathPrefix: `/`,
 	title: `hpp Portfolio`, // タイトル
-	titleAlt: `My Wonderful Website`, // JSONLDのためのタイトル
-	description: `Welcome to my brilliant website.`,
+	titleAlt: `hpp's wonderful portfolio`, // JSONLDのためのタイトル
+	description: `hpp is a Japanese student majoring in computer science and Informatics at Nagoya uni.`,
 	url: `https://hpprc.com`, // スラッシュなしのサイトURL
 	siteUrl: `https://hpprc.com/`, // スラッシュありのサイトURL
 	siteLanguage: `ja`, // HTMLの言語（ここでは日本語）
 	icon: `src/images/icon.png`,
 	logo: `src/images/icon.png`,
 	banner: `src/images/icon.png`,
-	favicon: `src/images/icon.png`,
-	shortName: `hppPortfolio`, // サイトの略称、12文字以下
+	shortName: `hppRC`, // サイトの略称、12文字以下
 	author: `hpprc`,
 	themeColor: `#3e7bf2`,
 	backgroundColor: `#d3e0ff`,
