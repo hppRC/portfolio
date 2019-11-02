f39816dd4c6177e4a53e9b3a362d9e02147888e8
hppRC <hpp.ricecake@gmail.com>
2019-11-02 23:16:14 +0900

    なかなかかっこいい

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,39 +15,33 @@ export const Index = () => (
 	</Layout>
 );
 
-const items = ['Lorem', 'ipsum', 'dolor', 'sit'];
-const config = { mass: 5, tension: 2000, friction: 200 };
+const items = [<AboutMDX />, <IntroMDX />, <ProjectsMDX />];
+const config = { mass: 2, tension: 2000, friction: 200 };
 
 const Intro = () => {
-	const [toggle, set] = useState(true);
 	const trail = useTrail(items.length, {
 		config,
-		opacity: toggle ? 1 : 0,
-		x: toggle ? 0 : 20,
-		height: toggle ? 80 : 0,
+		opacity: 1,
+		x: 0,
+
 		from: { opacity: 0, x: 20, height: 0 }
 	});
 
 	return (
-		<div className='trails-main' onClick={() => set(state => !state)}>
-			<div>
-				{trail.map(({ x, height, ...rest }, index) => (
-					<animated.div
-						key={items[index]}
-						className='trails-text'
-						style={{
-							...rest,
-							transform: x.interpolate(
-								x => `translate3d(0,${x}px,0)`
-							)
-						}}
-					>
-						<animated.div style={{ height }}>
-							{items[index]}
-						</animated.div>
+		<div>
+			{trail.map(({ x, height, ...rest }, index) => (
+				<animated.div
+					key={index}
+					style={{
+						...rest,
+						transform: x.interpolate(x => `translate3d(0,${x}px,0)`)
+					}}
+				>
+					<animated.div style={{ color: 'white' }}>
+						{items[index]}
 					</animated.div>
-				))}
-			</div>
+				</animated.div>
+			))}
 		</div>
 	);
 };
