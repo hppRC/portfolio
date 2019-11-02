21c2f17af7202149fc690045b3cda4625a9a4d75
hppRC <hpp.ricecake@gmail.com>
2019-11-02 22:56:27 +0900

    react-springを試す

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Layout } from '../layouts';
 import {
 	AboutMDX,
@@ -6,14 +6,50 @@ import {
 	ProjectsMDX,
 	BackgroundArt
 } from '../sections/index';
+import { useTrail, animated } from 'react-spring';
 
 export const Index = () => (
 	<Layout>
 		<BackgroundArt />
-		<AboutMDX />
-		<IntroMDX />
-		<ProjectsMDX />
+		<Intro />
 	</Layout>
 );
+
+const items = ['Lorem', 'ipsum', 'dolor', 'sit'];
+const config = { mass: 5, tension: 2000, friction: 200 };
+
+const Intro = () => {
+	const [toggle, set] = useState(true);
+	const trail = useTrail(items.length, {
+		config,
+		opacity: toggle ? 1 : 0,
+		x: toggle ? 0 : 20,
+		height: toggle ? 80 : 0,
+		from: { opacity: 0, x: 20, height: 0 }
+	});
+
+	return (
+		<div className='trails-main' onClick={() => set(state => !state)}>
+			<div>
+				{trail.map(({ x, height, ...rest }, index) => (
+					<animated.div
+						key={items[index]}
+						className='trails-text'
+						style={{
+							...rest,
+							transform: x.interpolate(
+								x => `translate3d(0,${x}px,0)`
+							)
+						}}
+					>
+						<animated.div style={{ height }}>
+							{items[index]}
+						</animated.div>
+					</animated.div>
+				))}
+			</div>
+		</div>
+	);
+};
 
 export default Index;
--- src/sections/index/backbround-art.js
+++ src/sections/index/backbround-art.js
@@ -9,7 +9,7 @@ const theme = css`
 	left: 0;
 	width: 100vw;
 	height: 100vh;
-	background-color: #0b0b0b;
+	background-color: #000;
 	z-index: -9999;
 `;
 
@@ -23,20 +23,20 @@ varying vec3 pos;
 void main() {
 	vColor = color;
 	pos = vec3(
-		position.x + clamp(
+		(sin(time * .53) + cos(time * 0.03 + 32.) + 1.) * position.x + sin(time * .1 - 200.) * clamp(
 			1.,
-			sin(time),
+			sin(time * 0.2),
 			position.x * ( 1. / time + cos(position.x) * position.x * sin(1. + position.z * time) * cos(1.2 * position.x * sin(cos(position.z + 0.01 * sin(0.5 * time))) + sin(cos(0.5 * time) * position.y)))
 		),
-		position.y +
-			position.y * (1. / time + sin(position.z + 0.5 * time) * position.y * cos(0.001*0.5 * time + position.z + sin(cos(sin(1. - sin(0.5 * time) * cos(0.5 * time)) + sin(0.5 * time)) + sin(cos(0.5 * time)) + 0.5 * time * sin(position.y))) * cos(position.x * cos(sin(0.5 * time)))
+		(cos(time * .23 - position.x) + 1.) * position.y +
+			position.y * (1. / time + sin(position.z + 0.5 * time) * position.y * cos(0.001*0.5 * time + position.z + sin(cos(sin(1. - sin(0.5 * time) * cos(0.5 * time)) + sin(0.5 * time)) + sin(cos(0.5 * time)) + 0.5 * time * sin(position.y))) * cos(position.x * cos(sin(0.23 * time)))
 		),
 		position.z + min(
 			sin(time * 0.4 - 200.),
-			position.z * (1. / abs(0.5 * time - 100.) + cos(0.0025*0.5 * time) * position.z + sin(sin(position.y * sin(cos(0.5 * time) + 0.5 * time * sin(position.y)) * cos(position.x * cos(sin(0.5 * time))))))
+			position.z * (1. / abs(0.5 * time - 100.) + cos(0.0025*0.5 * time) * position.z + sin(sin(position.y * sin(cos(0.5 * time) + 0.5 * time * sin(position.y)) * cos(position.x * cos(sin(0.3218 * time - 2.2))))))
 		)
 			);
-	gl_PointSize = 1.5;
+	gl_PointSize = 1.;
 	gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
 `;
@@ -74,14 +74,15 @@ const Thing = () => {
 			x = Math.random() * 2.0 - 1.0;
 			y = Math.random() * 2.0 - 1.0;
 			z = Math.random() * 2.0 - 1.0;
-
-			positions.push(x);
-			positions.push(y);
-			positions.push(z);
-			colors.push(Math.random() * 255.0);
-			colors.push(Math.random() * 255.0);
-			colors.push(Math.random() * 255.0);
-			colors.push(Math.random() * 255.0);
+			if (x * x + y * y + z * z <= 1) {
+				positions.push(x);
+				positions.push(y);
+				positions.push(z);
+				colors.push(Math.random() * 255.0);
+				colors.push(Math.random() * 255.0);
+				colors.push(Math.random() * 255.0);
+				colors.push(Math.random() * 255.0);
+			}
 		}
 		return [positions, colors];
 	}, []);
