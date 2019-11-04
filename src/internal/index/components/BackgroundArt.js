228cb2ab892c1788ea8a09bd3a361d87a4c2ab88
hppRC <hpp.ricecake@gmail.com>
2019-11-04 15:18:03 +0900

    clamp消してみた

--- src/internal/index/components/BackgroundArt.js
+++ src/internal/index/components/BackgroundArt.js
@@ -14,8 +14,7 @@ varying vec3 pos;
 void main() {
 	vColor = color;
 	pos = vec3(
-		(sin(time * .053) + cos(time * 0.03 + 32.) + 1.) * position.x + sin(time * .1 - 200.) * clamp(
-			1.,
+		(sin(time * .053) + cos(time * 0.03 + 32.) + 1.) * position.x + sin(time * .1 - 200.) * min(
 			sin(time * 0.2),
 			position.x * ( 1. / time + cos(position.x) * position.x * sin(1. + position.z * time) * cos(1.2 * position.x * sin(cos(position.z + 0.01 * sin(0.5 * time))) + sin(cos(0.5 * time) * position.y)))
 		),
@@ -61,7 +60,7 @@ const Thing = () => {
 		let positions = [];
 		let colors = [];
 		let x, y, z;
-		const points = isBrowser ? 50000 : 3000;
+		const points = isBrowser ? 50000 : 10000;
 		for (let i = 0; i < points; i++) {
 			x = Math.random() * 2.0 - 1.0;
 			y = Math.random() * 2.0 - 1.0;
