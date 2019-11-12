e6ed6dabf26235db4dca5c3f05d76759b46354ee
hppRC <hpp.ricecake@gmail.com>
2019-11-12 17:13:43 +0900

    作品の各写真を載せるcardの作成

---
+++ src/internal/works/components/Card.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Card = styled.div`
+	display: flex;
+
+	padding: 2rem 0;
+	border-radius: 5px;
+	img {
+		width: 100%;
+		height: auto;
+	}
+`;
+export default Card;
--- src/internal/works/components/works.tsx
+++ src/internal/works/components/works.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
 import WorksMDX from '../documents/works.mdx';
 import styled from '@emotion/styled';
-import Test from '../gif/glitch.gif';
 
 const Theme = styled.div``;
 
 export const Works = () => (
 	<Theme>
 		<WorksMDX />
-		<img src={Test} alt='' />
 	</Theme>
 );
 
