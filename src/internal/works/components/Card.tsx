fb1c8dc10ab7594dfc57d04e5c49e77f92d9bd85
hppRC <hpp.ricecake@gmail.com>
2019-11-12 17:31:41 +0900

    cardのstyling

--- src/internal/works/components/Card.tsx
+++ src/internal/works/components/Card.tsx
@@ -1,13 +1,29 @@
 import styled from '@emotion/styled';
 
-export const Card = styled.div`
-	display: flex;
-
-	padding: 2rem 0;
-	border-radius: 5px;
+const Theme = styled.div`
+	color: #fff;
 	img {
 		width: 100%;
 		height: auto;
+		border-radius: 5px;
 	}
 `;
+
+const StyledA = styled.a`
+	cursor: pointer;
+	text-decoration: none;
+`;
+
+const StyledLi = styled.li`
+	list-style: none;
+`;
+
+export const Card: React.FC<{ to: string }> = ({ to, children }) => (
+	<StyledLi>
+		<StyledA href={to}>
+			<Theme>{children}</Theme>
+		</StyledA>
+	</StyledLi>
+);
+
 export default Card;
--- src/internal/works/components/works.tsx
+++ src/internal/works/components/works.tsx
@@ -1,7 +1,18 @@
 import WorksMDX from '../documents/works.mdx';
 import styled from '@emotion/styled';
 
-const Theme = styled.div``;
+const Theme = styled.div`
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	width: 100%;
+	grid-gap: 3rem;
+
+	@media screen and (max-width: 768px) {
+		grid-template-columns: repeat(1, 1fr);
+	}
+	@media screen and (max-width: 480px) {
+	}
+`;
 
 export const Works = () => (
 	<Theme>
